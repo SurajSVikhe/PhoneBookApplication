92b86be9beb10bc791f5f3da453e8ee988d29dbd
SurajSVikhe <vikhesuraj52@gmail.com>
2022-12-16 18:57:08 +0530

    PBA-Phone Book Application

---
+++ src/main/java/com/BikkadIT/Controller/ContectController.java
@@ -0,0 +1,8 @@
+package com.BikkadIT.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContectController {
+
+}
---
+++ src/main/java/com/BikkadIT/Service/ContectServiceImpl.java
@@ -0,0 +1,8 @@
+package com.BikkadIT.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContectServiceImpl {
+
+}
